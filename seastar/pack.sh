f2ce2a5e40ba079095efa02e24ddff253f2f9dab
sunzhenkai <zhenkai.sun@qq.com>
2022-02-07 13:29:00 +0800

    update seastar

--- seastar/pack.sh
+++ seastar/pack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-COMMIT_ID=1433623962e6abca03dd23ebd1909f9b1a4fce2a
-bash "$BASE/../scripts/pack_git_repo.sh" -r https://gitee.com/mirrors/seastar.git -v seastar-submodule-${COMMIT_ID} \
+COMMIT_ID=368865a2b867922655179f9f2249a8ac4b15854f
+#bash "$BASE/../scripts/pack_git_repo.sh" -r https://gitee.com/mirrors/seastar.git -v seastar-submodule-${COMMIT_ID} \
+bash "$BASE/../scripts/pack_git_repo.sh" -r https://github.com/scylladb/seastar.git -v seastar-submodule-${COMMIT_ID} \
   -c ${COMMIT_ID}
