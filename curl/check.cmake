4a8d9296e371fb25b45e29328427a845272b689e
wii <zhenkai.sun@qq.com>
2022-04-09 17:30:42 +0800

    fix utils.cmake

--- curl/check.cmake
+++ curl/check.cmake
@@ -5,8 +5,8 @@ function(Process)
             DEP_PROJECT ${_DEP_NAME}
             DEP_TAG ${_DEP_NAME}-${_DEP_VER_}
             OSS_FILE ${_DEP_NAME}-${_DEP_NAME}-${_DEP_VER_}.tar.gz
-            CONFIGURE_DEFINE --with-openssl --enable-shared=no
-            AUTO_RE_CONF MAKE INSTALL)
+            CONFIGURE_DEFINE --with-openssl --enable-shared=no --disable-ldap --disable-ldaps --without-brotli --without-libidn --without-libidn2
+            AUTO_RE_CONF CONFIGURE MAKE INSTALL)
 endfunction(Process)
 Process()
 ProcessAddLibrary()
