0b4b150690ffb7b7bf00d55567d17b7237a68667
sunzhenkai <zhenkai.sun@qq.com>
2022-04-18 20:20:24 +0800

    add json

---
+++ json/check.cmake
@@ -0,0 +1,11 @@
+function(Process)
+    PrepareDeps(3.10.5 MODULES json)
+    AddProject(
+            DEP_AUTHOR nlohmann
+            DEP_PROJECT ${_DEP_NAME}
+            DEP_TAG v${_DEP_VER}
+            OSS_FILE ${_DEP_NAME}-${_DEP_VER}.tar.gz
+            NINJA)
+endfunction(Process)
+Process()
+ProcessAddLibrary()
