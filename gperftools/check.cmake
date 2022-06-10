48d2092b4249bfb5e9adafc426a3544557d1dcb3
sunzhenkai <zhenkai.sun@qq.com>
2022-06-10 16:52:31 +0800

    add gperftools

---
+++ gperftools/check.cmake
@@ -0,0 +1,11 @@
+function(Process)
+    PrepareDeps(2.10 MODULES tcmalloc tcmalloc_minimal tcmalloc_and_profiler)
+    AddProject(
+            DEP_AUTHOR ${_DEP_NAME}
+            DEP_PROJECT ${_DEP_NAME}
+            DEP_TAG v${_DEP_VER}
+            OSS_FILE ${_DEP_NAME}-${_DEP_VER}.tar.gz
+            NINJA)
+endfunction(Process)
+Process()
+ProcessAddLibrary()
