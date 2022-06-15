d6f00fd1f7086cf29fd462dde5e8f2f64f466b0d
sunzhenkai <zhenkai.sun@qq.com>
2022-06-15 19:14:44 +0800

    add brpc

---
+++ incubator-brpc/check.cmake
@@ -0,0 +1,17 @@
+include(${CMAKE_CURRENT_LIST_DIR}/../gflags/check.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../leveldb/check.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../thrift/check.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../boost/check.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../snappy/check.cmake)
+
+function(Process)
+    PrepareDeps(1.1.0 MODULES brpc)
+    AddProject(
+            DEP_AUTHOR apache
+            DEP_PROJECT ${_DEP_NAME}
+            DEP_TAG v${_DEP_VER}
+            OSS_FILE ${_DEP_NAME}-${_DEP_VER}.tar.gz
+            NINJA)
+endfunction(Process)
+Process()
+ProcessAddLibrary()
