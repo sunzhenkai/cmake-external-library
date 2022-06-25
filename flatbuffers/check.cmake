09dcf342037d2cc46249af5b49bca77a9df71958
wii <zhenkai.sun@qq.com>
2022-06-25 15:28:03 +0800

    fix flatbuffers

--- flatbuffers/check.cmake
+++ flatbuffers/check.cmake
@@ -11,24 +11,24 @@ ProcessAddLibrary(EXECUTABLES flatc)
 macro(GenerateFlatbuffersMessage target)
     cmake_parse_arguments(ARG "" "OUTPUT;PATH" "FILES" ${ARGN})
 
-    file(GLOB T_FB_FILES ${P_FILES})
+    file(GLOB T_FB_FILES ${ARG_FILES})
     get_property(flatbuffers_binary TARGET flatbuffers::bin::flatc PROPERTY LOCATION)
     foreach (I ${T_FB_FILES})
         execute_process(COMMAND ${flatbuffers_binary} --cpp --scoped-enums --reflect-names --gen-object-api
-                -o ${P_OUTPUT} ${I}
+                -o ${ARG_OUTPUT} ${I}
                 RESULT_VARIABLE rc)
         if (NOT "${rc}" STREQUAL "0")
             message(FATAL_ERROR "[GenerateFlatbuffersMessage] generate ${I} cpp code failed. [message=${rc}]")
         endif ()
-        execute_process(COMMAND ${flatbuffers_binary} --binary --schema -o ${P_OUTPUT} ${I}
+        execute_process(COMMAND ${flatbuffers_binary} --binary --schema -o ${ARG_OUTPUT} ${I}
                 RESULT_VARIABLE rc)
         if (NOT "${rc}" STREQUAL "0")
             message(FATAL_ERROR "[GenerateFlatbuffersMessage] generate ${I} binary failed. [message=${rc}]")
         endif ()
     endforeach ()
-    set(${target}_include ${P_OUTPUT})
-    file(GLOB ${target}_src ${P_OUTPUT}/*.h)
-    file(GLOB ${target}_binary ${P_OUTPUT}/*.bfbs)
+    set(${target}_include ${ARG_OUTPUT})
+    file(GLOB ${target}_src ${ARG_OUTPUT}/*.h)
+    file(GLOB ${target}_binary ${ARG_OUTPUT}/*.bfbs)
     include_directories(${${target}_include})
 
     DoUnset(TARGETS T_FB_FILES ARG_OUTPUT ARG_PATH ARG_FILES proto_binary)
