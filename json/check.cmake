697ae17781cda162415ab40ab9a6a020f8bc737f
zhenkai.sun <zhenkai.sun@qq.com>
2022-08-06 00:40:14 +0800

    fix json

--- json/check.cmake
+++ json/check.cmake
@@ -4,8 +4,9 @@ function(Process)
     PrepareDep(3.10.5 FIND_BY_HEADER FIND_SUFFIX include/nlohmann MODULES json)
     DownloadDep(AUTHOR nlohmann TAG v${_DEP_VER} SPEED_UP_FILE ${_DEP_NAME}-${_DEP_VER}.tar.gz)
     Ninja()
+    PostProcess()
 endfunction(Process)
 Process()
 ProcessFindPackage(nlohmann_json)
 
-# link nlohmann_json
+# link nlohmann_json::nlohmann_json
